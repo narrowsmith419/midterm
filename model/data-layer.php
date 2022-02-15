ec379f524cc777c1d1ab911acaff6532f02493ea
Nathan Arrowsmith <arrowsmith.nathan@student.greenriver.edu>
2022-02-14 21:00:49 -0500

    added check box options populated by array in data-layer.php

---
+++ model/data-layer.php
@@ -0,0 +1,10 @@
+<?php
+    function getCondiments()
+    {
+        return array('This midterm is easy', 'I like midterms', 'Today is Monday');
+    }
+
+    function getMeals()
+    {
+        return array('breakfast', 'lunch', 'dinner');
+    }
